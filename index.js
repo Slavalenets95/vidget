268a0bac9005e74c3c464cdf45def3b37e6a3dd8
Slavalenets95 <62787096+Slavalenets95@users.noreply.github.com>
2021-06-17 09:19:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,102 +1,46 @@
 const menuList = document.querySelector('.fixed-menu__list')
 const primaryBtn = document.querySelector('.fixed-menu__btn.primary')
 const primaryItem = document.querySelector('.primary-item')
-const popupList = document.querySelector('.popup-list')
-let activePopup = null
+
 
 const closeMenuList = () => {
     menuList.dataset.opened = 'false'
     primaryItem.dataset.active = 'false'
 
-    if (popupList.dataset.opened === 'false') {
-        primaryBtn.addEventListener('mouseenter', primaryBtnMouseHandler)
-        primaryBtn.removeEventListener('click', primaryBtnClickHandler)
-        menuList.removeEventListener('click', menuListClickHandler)
-    }
-
-    if (popupList.dataset.opened === 'false' && menuList.dataset.opened === 'false') {
-        primaryBtn.addEventListener('mouseenter', primaryBtnMouseHandler)
-        primaryBtn.addEventListener('click', primaryBtnMouseHandler)
-        menuList.removeEventListener('click', menuListClickHandler)
-    }
-
-    if (popupList.dataset.opened === 'true') {
-        primaryBtn.addEventListener('click', primaryBtnMouseHandler)
-        primaryBtn.removeEventListener('mouseenter', primaryBtnMouseHandler)
-        menuList.removeEventListener('click', menuListClickHandler)
-    }
 }
+
 const openMenuList = () => {
     menuList.dataset.opened = 'true'
     primaryItem.dataset.active = 'true'
 
-    primaryBtn.addEventListener('click', primaryBtnClickHandler)
-    menuList.addEventListener('click', menuListClickHandler)
-    primaryBtn.removeEventListener('mouseenter', primaryBtnMouseHandler)
 }
 
-const openPopupList = (activeEl) => {
-    popupList.dataset.opened = 'true'
-    activeEl.dataset.active = 'true'
- 
-    popupList.addEventListener('click', closePopupHandler)
-}
 
-const closePopupList = (activeEl) => {
-    popupList.dataset.opened = 'false'
-    activeEl.dataset.active = 'false'
-    popupList.removeEventListener('click', closePopupHandler)
-}
-
-const primaryBtnMouseHandler = () => {
-    if (popupList.dataset.opened === 'false' && menuList.dataset.opened === 'false') {
-        openMenuList()
-    }
-}
-
-const primaryBtnClickHandler = () => {
-    if (popupList.dataset.opened === 'true') {
-        openMenuList()
-        closePopupList(activePopup)
-        return
-    }
-    else if (menuList.dataset.opened === 'true'){
+const primaryBtnHandler = () => {
+     if (menuList.dataset.opened === 'true'){
         closeMenuList()
+        primaryBtn.addEventListener('click', primaryBtnHandler)
+        primaryBtn.addEventListener('mouseenter', primaryBtnHandler)
     }
     else {
         openMenuList()
+        primaryBtn.addEventListener('click', primaryBtnHandler)
+        primaryBtn.removeEventListener('mouseenter', primaryBtnHandler)
     } 
 }
 
 
 const menuListClickHandler = (e) => {
-    if (e.target.closest('.fixed-menu__list-link') && e.target.closest('.fixed-menu__list-link').dataset.target) {
-        const currentElem = e.target.closest('.fixed-menu__list-link')
-        activePopup = document.querySelector(`${currentElem.dataset.target}`)
-        e.preventDefault()
-        openPopupList(activePopup)
         closeMenuList()
-    }
 }
 
-
-const closePopupHandler = (e) => {
-    if (e.target.closest('.popup-list__close-btn')) {
-        const item = e.target.closest('.popup-list__item')
-        closePopupList(item)
-        primaryBtn.removeEventListener('click', primaryBtnClickHandler)
-        if(window.screen.availWidth <= 768) {
-            primaryBtn.addEventListener('click', primaryBtnMouseHandler)
-        } else {
-            primaryBtn.addEventListener('mouseenter', primaryBtnMouseHandler)
-        }
-    }
-}
 
 if(window.screen.availWidth <= 768) {
-    primaryBtn.addEventListener('click', primaryBtnMouseHandler)
+    primaryBtn.addEventListener('click', primaryBtnHandler)
 } else {
-    primaryBtn.addEventListener('mouseenter', primaryBtnMouseHandler)
+    primaryBtn.addEventListener('mouseenter', primaryBtnHandler)
 }
+
+
 
 
